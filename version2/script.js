5b6cbc1cc423eac2f2b75cd5668f2740953b03a6
Savenok Igor <camelactive@inbox.ru>
2023-10-15 20:25:32 +0500

    Проверка запуска кнопки лифта на начальном екране

--- version2/script.js
+++ version2/script.js
@@ -2,14 +2,17 @@ let callButtonDoor = document.querySelector("elevator-control-panel-button"),
     leftDoor = document.querySelector(".elevator-left-door"),
     rightDoor = document.querySelector(".elevator-right-door");
 let doorsSound = new Audio("assets/sounds/soundscrate-elevator-door-open-1.mp3");
-
-function doorsOpen(){
-    leftDoor.style.transition = "width 3s";
-    rightDoor.style.transition = "width 3s";
-    leftDoor.style.width = "5%";
-    rightDoor.style.width = "5%";
-    doorsSound.play();
-    setTimeout(doorsClose,8000);
+statusChange();
+    function doorsOpen(){
+        if (status !== "inProgress"){
+        status = "inProgress";
+        leftDoor.style.transition = "width 3s";
+        rightDoor.style.transition = "width 3s";
+        leftDoor.style.width = "5%";
+        rightDoor.style.width = "5%";
+        doorsSound.play();
+        setTimeout(doorsClose,8000);
+    }
 }
 function doorsClose(){
     leftDoor.style.transition = "width 3s";
@@ -17,4 +20,8 @@ function doorsClose(){
     leftDoor.style.width = "50%";
     rightDoor.style.width = "50%";
     doorsSound.play();
+    setTimeout(statusChange,3000);
+}
+function statusChange(){
+    status = "ready";
 }
